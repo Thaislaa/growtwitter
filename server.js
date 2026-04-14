ffe89dca65b19d290ce04cb792234c4b06b7a5a3
Thaislaa <thaisladaveiga@gmail.com>
2026-04-14 08:05:10 -0300

    fix backend routes

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import express from 'express'
 import path from 'path'
 
+import { AuthRoutes } from './src/routes/AuthRoutes.js'
+import { UsersRoutes } from './src/routes/UsersRoutes.js'
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
 const distPath = path.resolve('dist')
 
+app.use(express.json())
+
+// BACKEND
+app.use('/auth', AuthRoutes.bind())
+app.use('/users', UsersRoutes.bind())
+
+// FRONTEND
 app.use(express.static(distPath))
 
 app.use((req, res) => {
